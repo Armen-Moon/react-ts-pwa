324591ee44fee793cf827ec2068e5f6e57e9f8b5
Armen Martirosyan <armenmartirosyan1998@gmail.com>
2020-04-10 23:03:08 +0400

    Updates

--- precache-manifest.1c3995c05cd263ed41a4978739cbd1bd.js
+++ precache-manifest.1c3995c05cd263ed41a4978739cbd1bd.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e227ebac2d1e57757886b9c9a544d5eb",
+    "revision": "05fcac399df48054494f00ecacb29f8f",
     "url": "/react-ts-pwa/index.html"
   },
   {
-    "revision": "7b570803d43a885d0ade",
+    "revision": "b4ea0947d2314864b876",
     "url": "/react-ts-pwa/static/css/main.e3c3a6b2.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-ts-pwa/static/js/2.3ea95dcf.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "7b570803d43a885d0ade",
-    "url": "/react-ts-pwa/static/js/main.38306543.chunk.js"
+    "revision": "b4ea0947d2314864b876",
+    "url": "/react-ts-pwa/static/js/main.4b094dcf.chunk.js"
   },
   {
     "revision": "d4467710876a73fc8cbc",
