5fbbbca94850af6a57b107a5cc23fd9207c904da
Armen Martirosyan <armenmartirosyan1998@gmail.com>
2020-04-10 22:17:37 +0400

    init

---
+++ src/test/index.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+interface Props {
+    count?: number;
+}
+
+const Test: React.FunctionComponent<Props> = (props) => {
+    return (
+        <>
+            {/* <img src="https://images.unsplash.com/photo-1586468801912-2b17c93527f0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80" alt="" /> */}
+        </>
+    );
+};
+
+export default Test;
