fdbcf0319954ed5db4f0373e2653c5e395704441
unknown <armenmartirosyan1998@gmail.com>
2020-05-04 12:29:27 +0400

    feat(global): add commitizen

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Test from './test';
+import { version } from '../package.json';
 
 function App() {
+
+  console.log('version =>', version);
+
   return (
     <Test />
   );
